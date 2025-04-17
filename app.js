00dc0653cf49fcd586d491b383ba9af8436c02fc
Harry <117903881+soundjuggler@users.noreply.github.com>
2025-04-17 09:48:51 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -23,6 +23,32 @@ app.get("/request", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "request.html"));
 });
 
+// Add these to your existing app.js
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "about.html"));
+});
+
+app.get("/contact", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "contact.html"));
+});
+
+app.get("/categories", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "categories.html"));
+});
+
+app.get("/events", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "events.html"));
+});
+
+app.get("/checkout", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "checkout.html"));
+});
+
+app.get("/history", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "history.html"));
+});
+
+
 //START SERVER COMMAND, PORT IS DEFINED ABOCVE SO ITS CHANGED ABOVE IT WILL CHANGE HERER
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
